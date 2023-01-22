7435f5f649a79effef2ebf04e16e9f6956eb5dff
Dawid <daw.parc@gmail.com>
2023-01-22 19:29:44 +0100

    Add styles for buttons

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import Button from '../Button/Button';
+import styles from './Navigation.module.scss';
+
+const Navigation = () => {
+    return (
+      <div className={styles.navigation}>
+        <Button>start</Button>
+        <Button>stop</Button>
+        <Button>reset</Button>
+      </div>
+    );
+  };
+
+  export default Navigation;
